d3612b569693e2a5efebefc35e706d551077163f
vinicin <zviniciusnascimento@gmail.com>
2025-06-25 11:46:32 -0300

    initial commit

---
+++ pomodoro_app/bin/pomodoro_app.dart
@@ -0,0 +1,5 @@
+import 'package:pomodoro_app/pomodoro_app.dart' as pomodoro_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pomodoro_app.calculate()}!');
+}
---
+++ pomodoro_app/test/pomodoro_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:pomodoro_app/pomodoro_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
